b3a875fae98e9fca7ac43a7c458dfe78af6db0aa
ThinkK1ng <cla.givens@gmail.com>
2023-03-28 18:52:33 -0500

    chakra ui, header component changes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./components/Header";
+import Navtabs from "./components/navtabs";
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
+    <ChakraProvider>
     <div className="container">
       <Header />
+      <Navtabs />
     </div>
+    </ChakraProvider>
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,24 @@
 import React from "react";
+// imported default user pfp
 import { FaUser } from "react-icons/fa";
 
-const Header = () => {
+import { Box } from "@chakra-ui/react";
+
+const Header = ({ currentPage, handlePageChange }) => {
   return (
     <div>
-      <h1>Rappor</h1>
+      <h1>
+        <Box color="red">Rappor</Box>
+      </h1>
+      {/* turned FaUser icon into a button that takes user to profile page. */}
       <button>
-        <FaUser />
+        <a
+          href="#profile"
+          onClick={() => handlePageChange("Profile")}
+          className={currentPage === "Profile" ? "nav-link active" : "nav-link"}
+        >
+          <FaUser />
+        </a>
       </button>
     </div>
   );
