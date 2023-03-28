db55330d4c979a64574baa7946a18c8f13f2c931
Bonee94 <bonee94@yahoo.com>
2023-03-27 23:11:07 -0500

    Created subdocument comment schema for posts

---
+++ server/models/Comment.js
@@ -0,0 +1,22 @@
+const { Schema } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const commentSchema = new Schema({
+  commentText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  commentAuthor: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: String,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+});
+
+module.exports = commentSchema;
