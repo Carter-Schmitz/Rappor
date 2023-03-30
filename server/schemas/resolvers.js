8c09e9268cfb2ceceb597efc538bcf07bd75c193
Bonee94 <bonee94@yahoo.com>
2023-03-30 13:51:10 -0500

    addFriend mutation created for handling an accepted pending friend request

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,6 +19,7 @@ const resolvers = {
   
         return { token, user };
       },
+
       loginUser: async (parent, { email, password }) => {
         const user = await User.findOne({ email });
   
@@ -35,6 +36,37 @@ const resolvers = {
         const token = signToken(user);
   
         return { token, user };
+      },
+      //add friend mutation to be called after pending friend is accepted
+      addFriend: async (parent, {pendingId}, context) => {
+        console.log(pendingId)
+        // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+        if (context.user) {
+          //friend that sent request having current user added to their friend list
+          const pendingFriend = await User.findOneAndUpdate(
+            { _id:pendingId },
+            {
+              $addToSet: { friends: {friendId: context.user._id, friendUsername: context.user.username}  },
+            },
+            {
+              new: true,
+              runValidators: true,
+            }
+          );
+          
+          return User.findOneAndUpdate(
+            { _id: context.user._id },
+            {
+              $addToSet: {friends: {friendId: pendingId, friendUsername: pendingFriend.username}},
+            },
+            {
+              new: true,
+              runValidators: true,
+            }
+          );
+        }
+        // If user attempts to execute this mutation and isn't logged in, throw an error
+        throw new AuthenticationError('You need to be logged in!');
       },
     }
 }
