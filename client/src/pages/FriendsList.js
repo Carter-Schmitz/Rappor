f0e544ee6af7afc642a5dfff835a8f39447e9666
Carter_Schmitz <carterschmitz95@gmail.com>
2023-04-02 13:24:19 -0500

    buttons on friendslist

--- client/src/pages/FriendsList.js
+++ client/src/pages/FriendsList.js
@@ -1,41 +1,59 @@
 import { List, ListItem } from "@chakra-ui/layout";
 import { useMutation, useQuery, useLazyQuery } from "@apollo/client";
-import React, {useState, useEffect} from "react";
-import { QUERY_ME } from "../utils/queries";
-import { Box, Card, CardHeader, Flex, Avatar, Heading, CardBody, CardFooter, IconButton, Text } from "@chakra-ui/react";
-import { FaUser, FaEllipsisV } from 'react-icons/fa';
+import React, { useState, useEffect } from "react";
+import { QUERY_USER, QUERY_ME, QUERY_IS_FRIENDS } from "../utils/queries";
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  Flex,
+  Avatar,
+  Heading,
+  CardBody,
+  CardFooter,
+  IconButton,
+  Text,
+} from "@chakra-ui/react";
+import { FaUser, FaEllipsisV } from "react-icons/fa";
+import { ADD_FRIEND, ADD_PENDING } from '../utils/mutations';
+import  FriendArray  from "../components/friendArray/index";
 
-const FriendsList = () => {
-  const [FriendList, setFriends] = useState('');
+const FriendsList = ({username}) => {
+  const [FriendList, setFriends] = useState("");
 
-  const { data: me }= useQuery(QUERY_ME); 
-  const { loading, data: user } = useQuery(QUERY_ME); 
-  
-const handleChange = (event) => {
-  const { name, value } = event.target;
-}
+  const { data: me } = useQuery(QUERY_ME);
+  const { loading, data: user } = useQuery(QUERY_ME);
+
+  const { data: friendCheck } = useQuery(QUERY_IS_FRIENDS, {
+    variables: { username },
+  });
+
+  const [addPending, { error: pendingError }] = useMutation(ADD_PENDING, { 
+    refetchQueries: [
+      {query: QUERY_IS_FRIENDS,
+       variables: { username }},
+      ],
+      awaitRefetchQueries: true
+  });
+ 
+  const [addFriend, { error: addError }] = useMutation(ADD_FRIEND, {
+    refetchQueries: [
+      {query: QUERY_IS_FRIENDS,
+        variables: { username }},
+       ],
+       awaitRefetchQueries: true
+  });
+
   return (
     <Box>
       <h3>FriendsList</h3>
       {me?.me?.friends &&
-              me?.me?.friends.map((friend) => ( <Card key={friend.friendId}>
-                  <CardHeader>
-                      <Flex spacing="4">
-                          <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
-                              <Avatar>
-                                  <FaUser />
-                              </Avatar>
-                          </Flex>
-                          <IconButton variant="ghost" colorScheme="grey" aria-label="see menu"
-                              icon={<FaEllipsisV />} />
-                      </Flex>
-                  </CardHeader>
-                  <CardBody>
-                      <Text>{friend.friendUsername}</Text>
-                  </CardBody>
-        </Card> ))}
+        me?.me?.friends.map((friend) => (
+          <FriendArray username={friend.friendUsername} friendId={friend.friendId}></FriendArray>  
+        ))}
     </Box>
-  )
-}
+  );
+};
 
-export default FriendsList
+export default FriendsList;
