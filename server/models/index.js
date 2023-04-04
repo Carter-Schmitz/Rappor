8168c7ddb0c4ee1b88e90e99fbc20dc361913c35
Bonee94 <bonee94@yahoo.com>
2023-04-04 08:08:10 -0500

    deploying

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
-const User = require('./user');
 const Post = require('./Post');
+const User = require('./user');
 const MultiPost = require('./MultiPost');
 
 module.exports = { User, Post, MultiPost };
