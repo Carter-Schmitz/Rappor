112c516481e021c1c0b48a7356a16803664f225f
Bonee94 <bonee94@yahoo.com>
2023-04-04 07:16:26 -0500

    Adjusted App css

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import ParentContainer from "./components/parentContainer";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
 } from "@apollo/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { setContext } from "@apollo/client/link/context";
 import { ChakraProvider } from "@chakra-ui/react";
 
@@ -15,11 +14,12 @@ import SearchBar from "./components/searchBar";
 import NavTabs from "./components/navtabs/navtabs";
 
 import Feed from "./pages/Feed";
-import Profile from "./pages/Profile";
-import Login from "./pages/Login";
+import Profile from "./pages/Profile/Profile";
+import Login from "./pages/Login/Login";
 import FriendsList from "./pages/FriendsList";
 import Signup from "./pages/Signup";
 import Auth from "./utils/auth";
+import "./App.css"
 
 const httpLink = createHttpLink({
   uri: "/graphql",
@@ -50,20 +50,19 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <ChakraProvider>
-
-      <SearchBar></SearchBar>
       <Router>
       <NavTabs loggedIn={loggedIn}/>
         <>
           <Routes>
-            <Route path="/" element={<Login/>} />
-            <Route path="/feed" element={<Feed/>} />
-            <Route path="/me" element={<Profile />} />
-            <Route path="/friendslist" element={<FriendsList />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path={"/profiles" }>
+            {loggedIn ? <Route path="/" element={<Navigate to="/feed" />} /> : <Route path="/" element={<Login/>} />}
+            {loggedIn ? <Route path="/feed" element={<Feed/>} /> : <Route path="/feed" element={<Navigate to="/" />} />}
+            {loggedIn ? <Route path="/me" element={<Profile loggedIn={loggedIn} />} /> : <Route path="/me" element={<Navigate to="/" />} />}
+            {loggedIn ? <Route path="/friendsList" element={<FriendsList />} /> : <Route path="/friendsList" element={<Navigate to="/" />} />}
+            {loggedIn ? <Route path="/signup" element={<Navigate to="/me" />} /> : <Route path="/signup" element={<Signup />} />}
+            {loggedIn ? <Route path={"/profiles" }>
               <Route path=":username" element={<Profile />}/>
-            </Route>
+              </Route> 
+            : <Route path="/profiles" element={<Navigate to="/" />} />}
             <Route
               path="*"
               element={<h1 className="display-2">Wrong page!</h1>}
